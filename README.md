bd4b50a35de80eee24cced3b4dbf560d5355a463
ANTONOVA <89146019257@mail.ru>
2022-05-01 15:51:20 +0900

    add tests

---
+++ README.md
@@ -0,0 +1,2 @@
+2PostmanEcho
+[![Build status](https://ci.appveyor.com/api/projects/status/60ckhtesdyd78q03/branch/master?svg=true)](https://ci.appveyor.com/project/AntonovaAnastasiya/2postmanecho/branch/master)
--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,2 +1,77 @@
-package ru.netology.rest;public class PostmanEchoTest {
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldSendRequestBody() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldSendRequestBody2() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("12") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("12"))
+        ;
+    }
+
+    @Test
+    void shouldSendRequestBody3() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Нетология") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("Нетология"))
+        ;
+    }
+
+    @Test
+    void shouldSendRequestHeader() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Water") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("headers.content-length", equalTo("5"))
+        ;
+    }
 }
